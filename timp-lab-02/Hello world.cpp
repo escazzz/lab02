c84657436b19f9816b15972f6b16d7b9e69a2437
escazzz <escakhv@gmail.com>
2023-04-17 14:08:17 +0300

    Added comments

--- timp-lab-02/Hello world.cpp
+++ timp-lab-02/Hello world.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <string>
  
+
 int main(int argc, char** argv){
- string name;
- std::cin >> name;
+ string name; \\ User name
+ std::cin >> name; \\ Input user name
  std::cout << "Hello world from " << name << std::endl;
-}
+} 
