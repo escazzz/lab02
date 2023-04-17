0b725dd652c418d36e513136e82ce91469cc79e2
escazzz <escakhv@gmail.com>
2023-04-17 13:29:04 +0300

    Changed cpp file

--- timp-lab-02/Hello world.cpp
+++ timp-lab-02/Hello world.cpp
@@ -1,5 +1,10 @@
-#include <iostream>
-using namespace std;
-int main (int argc, char** argv){
-cout<<"Hello world"<<endl;
-}
+ #include <iostream>
+ #include <string>
+ 
+ using namespace std;
+ int main(int argc, char** argv){
+  string name;
+  cin >> name;
+  cout << "Hello world from " << name << endl;
+ }
+
